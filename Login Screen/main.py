4f9718363bb930e8c85b4129b96a28f6a1708cb2
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2021-12-08 06:48:52 +0000

    Update main.py

--- Login Screen/main.py
+++ Login Screen/main.py
@@ -6,19 +6,23 @@ screen = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 30)
 
-user_rect = Button(210, 90, 140, 32) # (x, y, width, height)
-pass_rect = Button(210, 140, 140, 32)   # width becomes 200 later
-email_rect = Button(210, 190, 140, 32)
+user_rect = Button(210, 90, 140, 32)  # (x, y, width, height)
+pass_rect = Button(210, 140, 140, 32)  # width becomes 200 later
+# email_rect = Button(210, 190, 140, 32)
+register_rect = Button(100, 210, 210, 32)
+login_rect = Button(350, 210, 210, 32)
 
-button_list = []    # So we can loop through all buttons easily
+button_list = []  # So we can loop through all buttons easily
 button_list.append(user_rect)
 button_list.append(pass_rect)
-button_list.append(email_rect)
+button_list.append(register_rect)
+button_list.append(login_rect)
+# button_list.append(email_rect)
 
 
 running = True
 while running:
-    screen.fill((0, 0, 0)) # Put it before you blit so it doesn't cover the text/images
+    screen.fill((0, 0, 0))  # Put it before you blit so it doesn't cover the text/images
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -28,12 +32,11 @@ while running:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             for button in button_list:  # Goes through all buttons
-                button.change_colour(mouse_pos) # Colour of box will be grey until clicked on
-
+                button.change_colour(mouse_pos)  # Colour of box will be grey until clicked on
 
         if event.type == pygame.KEYDOWN:  # Press a key
             for button in button_list:
-                if button.active == True:   # If the button has been clicked on
+                if button.active == True:  # If the button has been clicked on
                     button.get_text(screen, event)
 
     # Put labels onto screen
@@ -41,12 +44,18 @@ while running:
     screen.blit(username, (100, 100))
     password = font.render("Password", True, (255, 255, 255))
     screen.blit(password, (100, 150))
-    email = font.render("Email", True, (255, 255, 255))
-    screen.blit(email, (100, 200))
+    register = font.render("Register", True, (255, 255, 255))
+    screen.blit(register, (157, 217))
+    login = font.render("Login", True, (255, 255, 255))
+    screen.blit(login, (417, 217))
+    # email = font.render("Email", True, (255, 255, 255))
+    # screen.blit(email, (100, 200))
 
-    user_rect.draw(screen)  # To put the rectangles onto the screen
-    pass_rect.draw(screen)
-    email_rect.draw(screen)
+    for b in button_list:
+        if b == register_rect or b == login_rect:
+            b.draw2(screen)  # So it doesn't have the same functionalities as the text input boxes
+        else:
+            b.draw(screen)  # To put the rectangles onto the screen
 
     pygame.display.flip()
     clock.tick(60)
