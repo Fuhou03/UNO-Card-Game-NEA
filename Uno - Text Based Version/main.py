cea1fc6ded53226da57aefe9242df79372ad7028
Fuhou03 <68759675+Fuhou03@users.noreply.github.com>
2021-12-17 19:57:29 +0000

    Update main.py

--- Uno - Text Based Version/main.py
+++ Uno - Text Based Version/main.py
@@ -3,8 +3,11 @@ from player import Player
 from deck import Deck
 
 
-def display_deck(deck):
-    print("Your deck is: ")
+def display_deck(deck, player):
+    if player == 1:
+        print("Your deck is: ")
+    else:
+        print("\nThe opponent's deck: ") # For testing purposes
     for i in range(0, len(deck)):
         print(f"{i}: {deck[i].colour} - {deck[i].value}")
     print("\n", end="")
@@ -32,18 +35,69 @@ def play_game():
     discard_pile.append(dk.deck[0])  # Card at the top of the deck is placed down first
 
     turn = 1  # My turn (2 is the AI's turn)
-    for i in range(6):
+    finished = False
+    while not finished:
         if turn == 1:
             display_pile(discard_pile)
-            display_deck(me.deck)
-            discard_pile, dk.deck = me.choose_card(discard_pile, dk.deck)
-            turn = 2
-        else:
-            print("\nIt is the opponent's turn. \nThey have performed an action")
+            display_deck(me.deck, 1)
 
-            discard_pile, dk.deck = ai.select_card(discard_pile, dk.deck)
-            turn = 1
+            discard_pile, dk = me.choose_card(discard_pile, dk)
+
+            if me.drew_card == True:
+                turn = 2 # Moves onto opponent after I drew a card
+
+            elif discard_pile[-1].value == "skip" or discard_pile[-1] == "reverse":
+                turn = 1 # So the opponent's turn is skipped
+                print("The opponent's turn is skipped")
+
+            elif discard_pile[-1].value == "wild 4":
+                print("The opponent's turn is skipped \n")
+                for i in range(4):
+                    ai.deck = dk.draw_card(ai.deck)
+                turn = 1
+
+            elif discard_pile[-1].value == "draw 2":
+                for i in range(2):
+                    ai.deck = dk.draw_card(ai.deck)
+
+            else:
+                turn = 2
+
+
+
+        else:
+            display_deck(ai.deck, 2)
+            display_pile(discard_pile)
+            print("\nIt is the opponent's turn. \nThey have performed an action. \n")
+
+            discard_pile, dk = ai.select_card(discard_pile, dk)
+
+            if discard_pile[-1].value == "wild 4":
+                turn = 2  # My turn is skipped and I draw 4 cards
+                for i in range(4):
+                    me.deck = dk.draw_card(me.deck)
+                    print("Your turn is skipped")
+
+            elif discard_pile[-1].value == "draw 2":
+                for i in range(2):
+                    me.deck = dk.draw_card(me.deck)
+                turn = 2
+                print("Your turn is skipped")
+
+            elif discard_pile[-1].value == "skip" or discard_pile[-1].value == "reverse":
+                turn = 2
+                print("Your turn is skipped")
+
+            else:
+                turn = 1 # My turn
+
+        if len(me.deck) == 0:
+            finished = True
+            print("You won!")
+        elif len(ai.deck) == 0:
+            print("You lost!")
+        elif len(ai.deck) == 1:
+            print("Your opponent said: 'UNO!'")
 
 
 play_game()
-
